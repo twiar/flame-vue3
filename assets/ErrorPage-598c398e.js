03f6888774cb8586f199be2f234370cc0ef9817e
zhitenev.andr <zhitenev.andr@gmail.com>
2023-07-28 13:07:11 +0300

    adding dist

--- assets/ErrorPage-598c398e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,b as r,c}from"./index-69e497b3.js";const o={};function n(t,a){return r(),c("h1",null,"404 Not Found")}const _=e(o,[["render",n]]);export{_ as default};
---
+++ assets/ErrorPage-f73c60a7.js
@@ -0,0 +1,1 @@
+import{_ as e,b as r,c}from"./index-6ec0a73b.js";const o={};function n(t,a){return r(),c("h1",null,"404 Not Found")}const _=e(o,[["render",n]]);export{_ as default};
